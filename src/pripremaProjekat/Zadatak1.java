bb4bc8718cf1e1b2449d1e83b890102911ee0a55
Jelena Vacic <jelenavacic@gmail.com>
2021-02-18 21:15:49 +0100

    probni zadaci

---
+++ src/pripremaProjekat/Zadatak1.java
@@ -0,0 +1,12 @@
+package pripremaProjekat;
+
+public class Zadatak1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("proba 1");
+	}
+
+}
---
+++ src/pripremaProjekat/Zadatak2.java
@@ -0,0 +1,12 @@
+package pripremaProjekat;
+
+public class Zadatak2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("proba 2");
+
+	}
+
+}
